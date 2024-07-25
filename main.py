da1a500740e88b697a6be374361dfa2dc6955ab4
Bartosz Panek <m.panekbartosz@gmail.com>
2024-07-25 18:21:39 +0200

    Add event handler 'on_member_join'
    Event invoked when new member join server, it sends a welcome message along with a joke to a specified channel

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import discord
 from discord.ext import commands
-
-# Open the file in read mode
-with open('./token.txt', 'r') as file:
-    # Read the token from the file and strip any surrounding whitespace
-    token = file.read().strip()
+from api_keys import SERVER_TOKEN
+import requests
+import json
 
 # Define the intents required for the bot to read messages and execute commands
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 # Create an instance of the Bot with a command prefix '!' and the specified intents
 client = commands.Bot(command_prefix='!', intents=intents)
@@ -18,10 +17,33 @@ client = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print('BOT is ready for use!')
 
-# Define a command 'hello' which can be invoked with the command prefix (e.g., !hello)
+# Define a command 'hello/goodbye' which can be invoked with the command prefix (e.g., !hello)
 @client.command()
 async def hello(ctx):
     await ctx.send('Hello, I am the BOT')
 
+@client.command()
+async def goodbye(ctx):
+    await ctx.send('Goodybye, hope you come back soon')
+
+# Define an event handler 'on_member_join' which can be invoked when new member join server
+# It sends a welcome message along with a joke to a specified channel
+@client.event
+async def on_member_join(member):
+
+    jokeurl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+    response = requests.get(jokeurl)
+    response_json = response.json()
+
+    if 'joke' in response_json:
+        joke = response_json['joke']
+    else:
+        joke = 'I could not fetch a joke at the moment.'
+
+    channel = client.get_channel(315541123781099520)
+    await channel.send(f'Hello, {member.display_name}! Welcome to the server. Here is a joke for you:')
+    await channel.send(joke)
+
 # Run the bot with the token read from 'token.txt'
-client.run(token)
+client.run(SERVER_TOKEN)
